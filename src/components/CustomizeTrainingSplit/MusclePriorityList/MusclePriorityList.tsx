bfd6dc8d24a738b929d47bc1561cd89502c5317a
empower87 <mjamespower87@gmail.com>
2023-11-02 17:54:45 -0700

    creating UI for mesocycle frequency editing, very rudimentary and doesn't save atm, but will implement saving function tomorrow

--- src/components/CustomizeTrainingSplit/MusclePriorityList/MusclePriorityList.tsx
+++ src/components/CustomizeTrainingSplit/MusclePriorityList/MusclePriorityList.tsx
@@ -55,91 +55,210 @@ function Cell({ value, width }: CellProps) {
   return <div className=" w-1/12 indent-1">{value}</div>;
 }
 
+const useFrequencyEditor = (
+  mesoProgression: number[],
+  total: [number, number]
+) => {
+  const [canAddList, setCanAddList] = useState<boolean[]>([true, true, true]);
+  const [canSubList, setCanSubList] = useState<boolean[]>([true, true, true]);
+  const [currentMesoProgression, setCurrentMesoProgression] = useState<
+    number[]
+  >([...mesoProgression]);
+
+  const canAddHandler = (mesoProgression: number[], max: number) => {
+    const canAdds: boolean[] = [];
+    for (let i = 0; i < mesoProgression.length; i++) {
+      const currentElement = mesoProgression[i];
+      const nextElement = mesoProgression[i + 1];
+
+      if (nextElement) {
+        if (currentElement < nextElement) {
+          canAdds.push(true);
+        } else {
+          canAdds.push(false);
+        }
+      } else {
+        if (currentElement < max) {
+          canAdds.push(true);
+        } else {
+          canAdds.push(false);
+        }
+      }
+    }
+    return canAdds;
+  };
+
+  const canSubHandler = (mesoProgression: number[]) => {
+    const canSubs: boolean[] = [];
+    for (let i = 0; i < mesoProgression.length; i++) {
+      const currentElement = mesoProgression[i];
+      const prevElement = mesoProgression[i - 1];
+
+      if (prevElement) {
+        if (currentElement > prevElement) {
+          canSubs.push(true);
+        } else {
+          canSubs.push(false);
+        }
+      } else {
+        if (currentElement > 0) {
+          canSubs.push(true);
+        } else {
+          canSubs.push(false);
+        }
+      }
+    }
+    return canSubs;
+  };
+
+  const onAddSubHandler = (type: "add" | "sub", mesocycle: number) => {
+    const mesocycleIndex = mesocycle - 1;
+    let mesoProg = [...currentMesoProgression];
+
+    let newValue = mesoProg[mesocycleIndex] + 1;
+    if (type === "sub") {
+      newValue = mesoProg[mesocycleIndex] - 1;
+    }
+    mesoProg[mesocycleIndex] = newValue;
+
+    setCurrentMesoProgression(mesoProg);
+  };
+
+  const onCancelHandler = () => {
+    setCurrentMesoProgression([...mesoProgression]);
+  };
+
+  const onSaveHandler = () => {
+    console.log("saved");
+  };
+
+  useEffect(() => {
+    const max = total[0] + total[1];
+    const addBools = canAddHandler(currentMesoProgression, max);
+    const subBools = canSubHandler(currentMesoProgression);
+    setCanAddList(addBools);
+    setCanSubList(subBools);
+  }, [currentMesoProgression]);
+
+  return {
+    canAddList,
+    canSubList,
+    onAddSubHandler,
+    currentMesoProgression,
+  };
+};
+
 type CellWithCounterProps = {
   mesocycle: number;
-  mesoProgression: number[];
-  total_sessions: [number, number];
+  currentValue: number;
+  canAdd: boolean;
+  canSub: boolean;
+  onAddSubtract: (type: "add" | "sub", mesocycle: number) => void;
 };
 function CellWithCounter({
   mesocycle,
+  currentValue,
+  canAdd,
+  canSub,
+  onAddSubtract,
+}: CellWithCounterProps) {
+  return (
+    <div className=" mr-2 flex items-center justify-center">
+      <button
+        className={BG_COLOR_M6 + " h-4 w-4 text-xs font-bold"}
+        onClick={() => onAddSubtract("sub", mesocycle)}
+        disabled={!canSub}
+      >
+        -
+      </button>
+      <div className={" text-xxs p-1"}>{currentValue}</div>
+      <button
+        className={BG_COLOR_M6 + " h-4 w-4 text-xs font-bold"}
+        onClick={() => onAddSubtract("add", mesocycle)}
+        disabled={!canAdd}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+type EditMesocycleFrequencyProps = {
+  mesoProgression: number[];
+  total_sessions: [number, number];
+  onEdit: () => void;
+};
+function EditMesocycleFrequency({
   mesoProgression,
   total_sessions,
-}: CellWithCounterProps) {
-  const [canAdd, setCanAdd] = useState<boolean>(true);
-  const [canSub, setCanSub] = useState<boolean>(true);
-  const frequency =
-    mesocycle === 1
-      ? mesoProgression[0]
-      : mesocycle === 2
-      ? mesoProgression[1]
-      : mesoProgression[2];
-  const [currentValue, setCurrentValue] = useState<number>(frequency);
+  onEdit,
+}: EditMesocycleFrequencyProps) {
+  const { canAddList, canSubList, onAddSubHandler, currentMesoProgression } =
+    useFrequencyEditor(mesoProgression, total_sessions);
 
-  const totalSessions = total_sessions[0] + total_sessions[1];
-  const [minMax, setMinMax] = useState<[number, number]>([0, totalSessions]);
-
-  useEffect(() => {
-    setCurrentValue(frequency);
-  }, [frequency]);
-
-  useEffect(() => {
-    const maxFrequency =
-      mesocycle === 3
-        ? totalSessions
-        : mesocycle === 2
-        ? mesoProgression[2]
-        : mesoProgression[1];
-    const minFrequency =
-      mesocycle === 3
-        ? mesoProgression[2]
-        : mesocycle === 2
-        ? mesoProgression[0]
-        : 0;
-    setMinMax([minFrequency, maxFrequency]);
-  }, [totalSessions, mesocycle, mesoProgression]);
-
-  useEffect(() => {
-    if (currentValue > minMax[0]) {
-      setCanSub(true);
-    } else {
-      setCanSub(false);
-    }
-
-    if (currentValue < minMax[1]) {
-      setCanAdd(true);
-    } else {
-      setCanAdd(false);
-    }
-  }, [currentValue, minMax]);
-
-  const onClickHandler = (type: "add" | "sub") => {
-    if (type === "add") {
-      setCurrentValue((prev) => prev + 1);
-    } else {
-      setCurrentValue((prev) => prev - 1);
-    }
+  const cancel = () => {
+    onEdit();
   };
 
+  const save = () => {
+    onEdit();
+  };
   return (
-    <div className=" flex w-1/3 justify-center">
-      {canSub && (
+    <div className=" flex w-6/12">
+      <CellWithCounter
+        mesocycle={1}
+        currentValue={currentMesoProgression[0]}
+        canAdd={canAddList[0]}
+        canSub={canSubList[0]}
+        onAddSubtract={onAddSubHandler}
+      />
+      <CellWithCounter
+        mesocycle={2}
+        currentValue={currentMesoProgression[1]}
+        canAdd={canAddList[1]}
+        canSub={canSubList[1]}
+        onAddSubtract={onAddSubHandler}
+      />
+      <CellWithCounter
+        mesocycle={3}
+        currentValue={currentMesoProgression[2]}
+        canAdd={canAddList[2]}
+        canSub={canSubList[2]}
+        onAddSubtract={onAddSubHandler}
+      />
+      <div className=" flex items-center">
         <button
-          className={BG_COLOR_M6 + " p-1 text-xs font-bold"}
-          onClick={() => onClickHandler("sub")}
+          className={BG_COLOR_M5 + " mr-1 h-4 w-4 rounded-full font-bold"}
+          onClick={cancel}
         >
-          -
+          x
         </button>
-      )}
+        <button
+          className={"h-4 w-4 rounded-full bg-blue-600 font-bold"}
+          onClick={save}
+        >
+          ok
+        </button>
+      </div>
+    </div>
+  );
+}
 
-      <div className={" text-xxs p-1"}>{currentValue}</div>
-      {canAdd && (
-        <button
-          className={BG_COLOR_M6 + " p-1 text-xs font-bold"}
-          onClick={() => onClickHandler("add")}
-        >
-          +
-        </button>
-      )}
+function MesocycleFrequency({
+  mesoProgression,
+  onEdit,
+}: {
+  mesoProgression: number[];
+  onEdit: () => void;
+}) {
+  return (
+    <div className=" flex w-4/12">
+      <div className=" flex w-1/3 justify-center">{mesoProgression[0]}</div>
+      <div className=" flex w-1/3 justify-center">{mesoProgression[1]}</div>
+      <div className=" flex w-1/3 justify-center">{mesoProgression[2]}</div>
+      <button className="  bg-blue-600 pl-1 pr-1" onClick={onEdit}>
+        edit
+      </button>
     </div>
   );
 }
@@ -156,6 +275,7 @@ function Item({
   onVolumeChange,
   total_sessions,
 }: ItemProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
   const { mesoProgression, volume_landmark, muscle } = muscleGroup;
   const volumeSets = getVolumeSets(muscle, mesoProgression[2], volume_landmark);
   const mesoOneVolume = getEndOfMesocycleVolume(
@@ -179,11 +299,15 @@ function Item({
     onVolumeChange(index, value as VolumeLandmarkType);
   };
 
+  const onEditHandler = () => {
+    setIsEditing((prev) => !prev);
+  };
+
   return (
     <li className={bgColor + " text-xxs mb-0.5 flex p-0.5 text-white"}>
       <div className=" w-1/12 indent-1">{index + 1}</div>
 
-      <div className=" w-3/12 indent-1">{muscleGroup.muscle}</div>
+      <div className=" w-2/12 indent-1">{muscleGroup.muscle}</div>
 
       <div className=" flex w-2/12 justify-evenly">
         <Select
@@ -195,23 +319,18 @@ function Item({
         <div className=" ">{volumeSets}</div>
       </div>
 
-      <div className=" flex w-3/12">
-        <CellWithCounter
-          mesocycle={1}
-          mesoProgression={muscleGroup.mesoProgression}
+      {isEditing ? (
+        <EditMesocycleFrequency
+          mesoProgression={mesoProgression}
           total_sessions={total_sessions}
+          onEdit={onEditHandler}
         />
-        <CellWithCounter
-          mesocycle={2}
-          mesoProgression={muscleGroup.mesoProgression}
-          total_sessions={total_sessions}
+      ) : (
+        <MesocycleFrequency
+          mesoProgression={mesoProgression}
+          onEdit={onEditHandler}
         />
-        <CellWithCounter
-          mesocycle={3}
-          mesoProgression={muscleGroup.mesoProgression}
-          total_sessions={total_sessions}
-        />
-      </div>
+      )}
 
       <div className=" flex w-3/12">
         <div className=" flex w-1/3 justify-center">{mesoOneVolume}</div>
@@ -240,7 +359,7 @@ export function MusclePriorityList({
         <div className={BORDER_COLOR_M8 + " w-1/12 border-r-2 indent-1"}>
           Rank
         </div>
-        <div className={BORDER_COLOR_M8 + " w-3/12 border-r-2 indent-1"}>
+        <div className={BORDER_COLOR_M8 + " w-2/12 border-r-2 indent-1"}>
           Group
         </div>
         <div
@@ -248,7 +367,7 @@ export function MusclePriorityList({
         >
           Volume Benchmark
         </div>
-        <div className={BORDER_COLOR_M8 + " flex w-3/12 flex-col border-r-2"}>
+        <div className={BORDER_COLOR_M8 + " flex w-4/12 flex-col border-r-2"}>
           <div className=" mb-0.5 flex justify-center">Mesocycle Frequency</div>
           <div className=" flex">
             <div className=" flex w-1/3 justify-center">1</div>
