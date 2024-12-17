bbfccaa236d5215483191e964dd7c25a5ddafb19
Michael Power <mjamespower87@gmail.com>
2024-12-17 10:33:08 -0800

    adding a collapsible from shadcn to my SavedTrainingBlocks in workout page

--- src/pages/workout/SavedTrainingBlocks/SavedTrainingBlocks.tsx
+++ src/pages/workout/SavedTrainingBlocks/SavedTrainingBlocks.tsx
@@ -1,3 +1,8 @@
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "~/components/ui/collapsible";
 import { TrainingDayType } from "~/hooks/useTrainingProgram/reducer/trainingProgramReducer";
 import { useTrainingProgramContext } from "~/hooks/useTrainingProgram/useTrainingProgram";
 import { NewTrainingWeek } from "~/hooks/useTrainingProgram/utils/training_block/trainingBlockHelpers";
@@ -16,15 +21,68 @@ const createTrainingBlockData = (
   });
 };
 
+const TBLOCK_TEST: NewTrainingWeek[][] | TrainingDayType[][] = [
+  [
+    {
+      day: "Monday",
+      isTrainingDay: true,
+      sessions: [{ id: "0329", split: "upper", exercises: [] }],
+    },
+  ],
+  [],
+  [],
+];
 export default function SavedTrainingBlocks() {
   const { training_block } = useTrainingProgramContext();
 
+  const training_blocks = [TBLOCK_TEST];
+  console.log(training_block, "data here?");
   return (
     <div className="flex flex-col">
       <div>
         <h2>My Training Blocks</h2>
       </div>
-      <div></div>
+      <div>
+        <ul>
+          {training_blocks.map((block, index) => {
+            return <TrainingBlockItem index={index} training_block={block} />;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+type TrainingBlockItemProps = {
+  index: number;
+  training_block: NewTrainingWeek[][] | TrainingDayType[][];
+};
+function TrainingBlockItem({ index, training_block }: TrainingBlockItemProps) {
+  return (
+    <div className="flex ">
+      <Collapsible>
+        <CollapsibleTrigger>Training Block {index + 1}</CollapsibleTrigger>
+        <CollapsibleContent>
+          <Collapsible>
+            {training_block.map((meso, index) => {
+              return (
+                <>
+                  <CollapsibleTrigger>Mesocycle {index + 1}</CollapsibleTrigger>
+                  <CollapsibleContent>
+                    {meso.map((week, i) => {
+                      const hasSession = week.sessions[0]
+                        ? week.sessions[0].split
+                        : false;
+                      if (!hasSession) return null;
+                      return <div>{week.sessions[0].split}</div>;
+                    })}
+                  </CollapsibleContent>
+                </>
+              );
+            })}
+          </Collapsible>
+        </CollapsibleContent>
+      </Collapsible>
     </div>
   );
 }
--- src/pages/workout/index.tsx
+++ src/pages/workout/index.tsx
@@ -1,8 +1,12 @@
+import SavedTrainingBlocks from "./SavedTrainingBlocks/SavedTrainingBlocks";
+
 export default function Workout() {
   return (
     <div className="flex h-full flex-col pt-10">
       <h1 className="mb-5 text-white">Workout</h1>
-      <div className="flex h-full flex-col space-x-5 space-y-5"></div>
+      <div className="flex h-full flex-col space-x-5 space-y-5">
+        <SavedTrainingBlocks />
+      </div>
     </div>
   );
 }
